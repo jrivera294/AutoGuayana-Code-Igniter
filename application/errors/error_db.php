19aff45aa2ff1f15f6f94c976a4b11bf71527643
jrivera294 <jrivera294@gmail.com>
2014-06-15 19:14:17 -0430

    Mejora del manejo de errores de la base de datos

--- application/errors/error_db.php
+++ application/errors/error_db.php
@@ -10,9 +10,50 @@
     <link href="<?= base_url("assets/css/custom.css");?>" rel="stylesheet">
 </head>
 <body>
-  <div class="alert alert-danger">
-    <strong><?php echo $message; ?></strong>
+     <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="<?= base_url();?>index.php/">AutoGuayana</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="<?= base_url();?>index.php/clientes">Clientes</a></li>
+            <li><a href="<?= base_url();?>index.php/stock">Stock</a></li>
+            <li><a href="<?= base_url();?>index.php/facturar">Facturar</a></li>
+            <li><a href="<?= base_url();?>index.php/administracion">*Administración*</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
 
-  </div>
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+
+
+    <h2 class="sub-header">Error al registrar</h2>
+    <div class="alert alert-danger">
+        <strong>
+        <?php
+            if(strpos($message,"dom_nombre_check")){
+                echo "Nombres y apellidos deben tener la primera letra mayúscula y las demás en minúsculas";
+            }else if(strpos($message,"dom_fecha_nac")){
+                echo "Error en fecha de nacimiento. No puedes registrar a alguien que no ha nacido!";
+            }else if(strpos($message,"estatustrab_check")){
+                echo "Error en el estatus del trabajador, solo puede ser Activo,Permiso,Retirado,Vacaciones";
+            }else{
+                echo $message;
+            }
+        ?>
+        </strong>
+    </div>
+
+    <a href="javascript:history.go(-1)" class="btn btn-sm btn-primary" role="button">Volver</a>
+</div>
+
 </body>
 </html>
